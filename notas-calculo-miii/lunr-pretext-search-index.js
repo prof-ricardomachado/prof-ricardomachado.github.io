6bfe55c9f6d306b7168db3185ab654f85443b025
ricardo <ricardo@localhost.localdomain>
2024-05-31 10:45:49 -0300

    add notas calculo miii

---
+++ notas-calculo-miii/lunr-pretext-search-index.js
@@ -0,0 +1,283 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "prof-eudes",
+  "level": "1",
+  "url": "prof-eudes.html",
+  "type": "Biografia do autor",
+  "number": "",
+  "title": "Sobre Eudes Barboza",
+  "body": " Sobre Eudes Barboza  Professor Adjunto da Universidade Federal Rural de Pernambuco (UFRPE), doutor pelo Programa de Pós-Graduação em Matemática da UFPE, mestre pelo Programa de Mestrado em Matemática (UFPB), Graduado em Licenciado em Matemática e Comunicação Social (Relações Públicas) pela UFPB, medalhista da Iniciação científica PIBIC-UFPB e laureado na turma de Licenciatura em Matemática da UFRPE em 2011.1. Na UFRPE, coordenou o PROFMAT e projetos de ensino, pesquisa e extensão. Secretariou eventos nacionais e internacionais do DM-UFPB. e participou de comissões organizadoras e comitês científicos regionais em Pernambuco. Durante a graduação em Matemática foi monitor do PAPMEM, das Olimpíadas Pessoense de Matemática, do Programa de monitoria do DM-UFPB na disciplina de Cálculo Vetorial, posteriormente, da disciplina Introdução à Álgebra Linear, vinculado ao Programa de Mestrado em Matemática-UFPB. Foi tutor à distância em Matemática da EAD-UFPE. Tem experiência no ensino básico, em rede pública e privada. Tem atuado principalmente nos temas: Análise e EDP.  "
+},
+{
+  "id": "prof-thamires",
+  "level": "1",
+  "url": "prof-thamires.html",
+  "type": "Biografia do autor",
+  "number": "",
+  "title": "Sobre Thamires Cruz",
+  "body": " Sobre Thamires Cruz  Possui Graduação em Matemática Licenciatura pela Universidade Federal de Sergipe (2009), Mestrado(2012) e Doutorado(2016) em Matemática pela Universidade Federal de Pernambuco . Atualmente é professora adjunta da Universidade Federal Rural de Pernambuco. Sua principal área de atuação é equações diferenciais parciais de evolução.  "
+},
+{
+  "id": "prof-ricardo-machado",
+  "level": "1",
+  "url": "prof-ricardo-machado.html",
+  "type": "Biografia do autor",
+  "number": "",
+  "title": "Sobre Ricardo Machado",
+  "body": " Sobre Ricardo Machado  Professor Adjunto da Universidade Federal Rural de Pernambuco, DM.  Doutor em Matemática pela Universidade Federal de Pernambuco (2009-2013).  Mestre em Matemática pela Universidade Federal de Pernambuco (2007-2009).  Bacharel em Matemática pela Universidade Federal de Pernambuco (2003-2007).  "
+},
+{
+  "id": "preface",
+  "level": "1",
+  "url": "preface.html",
+  "type": "Prefácio",
+  "number": "",
+  "title": "Prefácio",
+  "body": "    Clessius Silva  Ricardo Machado  Recife, 2022   "
+},
+{
+  "id": "section-1",
+  "level": "1",
+  "url": "section-1.html",
+  "type": "Seção",
+  "number": "1.1",
+  "title": "O Espaço Euclidiano <span class=\"process-math\">\\(\\mathbb{R}^n\\)<\/span>",
+  "body": " O Espaço Euclidiano  O Espaço Euclidiano -dimensional é o produto cartesiano de fatores, com , iguais a  ou ainda, Assim, os pontos ou vetores do são representados por cujas coordenadas são números reais.   Dados em e , definimos as seguintes operações:   (Soma de vetores);   (Produto por escalar);   (Produto interno).      Definimos a norma euclidiana em por e assim em que é o ângulo entre os vetores e .    Funções vetoriais   Toda função do tipo com é chamada de função vetorial a uma variável real .  O domínio de é denotado por ;  é regra que associa cada número real a um único vetor ;  Cada com é denominada função componente ou função coordenada de .       Podemos representar por com  Já pode ser representada por com e .     ;  ;  ;     A imagem ou trajetória de uma função vetorial é o conjunto                                  Quando não for explicitado o domínio de uma função vetorial , consideraremos o domínio máximo em que esteja definida. Para isso, basta fazer a interseção dos domínios das funções componentes de .   Determine o domínio de .   Observe que , logo , e , com . Portanto,      Determine o domínio de .   Note que  ;     Portanto,      Em cada um dos itens abaixo determine e .     .    Esboce a trajetória de uma partícula , sabendo que seu movimento é descrito por:         Determine o domínio das seguintes funções vetoriais:            "
+},
+{
+  "id": "definition-1",
+  "level": "2",
+  "url": "section-1.html#definition-1",
+  "type": "Definição",
+  "number": "1.1.1",
+  "title": "",
+  "body": " Dados em e , definimos as seguintes operações:   (Soma de vetores);   (Produto por escalar);   (Produto interno).    "
+},
+{
+  "id": "remark-1",
+  "level": "2",
+  "url": "section-1.html#remark-1",
+  "type": "Nota",
+  "number": "1.1.2",
+  "title": "",
+  "body": " Definimos a norma euclidiana em por e assim em que é o ângulo entre os vetores e .  "
+},
+{
+  "id": "definition-2",
+  "level": "2",
+  "url": "section-1.html#definition-2",
+  "type": "Definição",
+  "number": "1.1.3",
+  "title": "",
+  "body": " Toda função do tipo com é chamada de função vetorial a uma variável real .  O domínio de é denotado por ;  é regra que associa cada número real a um único vetor ;  Cada com é denominada função componente ou função coordenada de .    "
+},
+{
+  "id": "remark-2",
+  "level": "2",
+  "url": "section-1.html#remark-2",
+  "type": "Nota",
+  "number": "1.1.4",
+  "title": "",
+  "body": " Podemos representar por com  Já pode ser representada por com e .  "
+},
+{
+  "id": "example-1",
+  "level": "2",
+  "url": "section-1.html#example-1",
+  "type": "Exemplo",
+  "number": "1.1.5",
+  "title": "",
+  "body": "  ;  ;  ;   "
+},
+{
+  "id": "definition-3",
+  "level": "2",
+  "url": "section-1.html#definition-3",
+  "type": "Definição",
+  "number": "1.1.6",
+  "title": "",
+  "body": " A imagem ou trajetória de uma função vetorial é o conjunto   "
+},
+{
+  "id": "example-2",
+  "level": "2",
+  "url": "section-1.html#example-2",
+  "type": "Exemplo",
+  "number": "1.1.7",
+  "title": "",
+  "body": "         "
+},
+{
+  "id": "example-3",
+  "level": "2",
+  "url": "section-1.html#example-3",
+  "type": "Exemplo",
+  "number": "1.1.10",
+  "title": "",
+  "body": "         "
+},
+{
+  "id": "example-4",
+  "level": "2",
+  "url": "section-1.html#example-4",
+  "type": "Exemplo",
+  "number": "1.1.13",
+  "title": "",
+  "body": "         "
+},
+{
+  "id": "example-5",
+  "level": "2",
+  "url": "section-1.html#example-5",
+  "type": "Exemplo",
+  "number": "1.1.16",
+  "title": "",
+  "body": " Determine o domínio de .   Observe que , logo , e , com . Portanto,    "
+},
+{
+  "id": "example-6",
+  "level": "2",
+  "url": "section-1.html#example-6",
+  "type": "Exemplo",
+  "number": "1.1.17",
+  "title": "",
+  "body": " Determine o domínio de .   Note que  ;     Portanto,    "
+},
+{
+  "id": "exercise-1",
+  "level": "2",
+  "url": "section-1.html#exercise-1",
+  "type": "Exercício",
+  "number": "1.1.2.1",
+  "title": "",
+  "body": "Em cada um dos itens abaixo determine e .     .   "
+},
+{
+  "id": "exercise-2",
+  "level": "2",
+  "url": "section-1.html#exercise-2",
+  "type": "Exercício",
+  "number": "1.1.2.2",
+  "title": "",
+  "body": "Esboce a trajetória de uma partícula , sabendo que seu movimento é descrito por:        "
+},
+{
+  "id": "exercise-3",
+  "level": "2",
+  "url": "section-1.html#exercise-3",
+  "type": "Exercício",
+  "number": "1.1.2.3",
+  "title": "",
+  "body": "Determine o domínio das seguintes funções vetoriais:          "
+},
+{
+  "id": "section-2",
+  "level": "1",
+  "url": "section-2.html",
+  "type": "Seção",
+  "number": "1.2",
+  "title": "Operações com Funções Vetoriais",
+  "body": " Operações com Funções Vetoriais   Sejam funções vetoriais tais que  e uma função real, então    está bem definida e  está bem definida e  está bem definida e   está bem definida e      Pelos itens e , se é uma constante real, temos: e Pelo item , temos ainda     Considere dadas por e e dada por Calcule:           Usando o SageMath para calcular o item iv do exemplo anterior     Esboce a trajetória de uma partícula , sabendo que seu movimento é descrito por:        Determine o domínio das seguintes funções vetoriais:           Considere duas funções vetoriais definidas em e uma função real dada por . Determine:          Considere duas funções vetoriais definidas em Determine   Sejam Calcule   Duas partículas se movem ao longo de duas curvas espaciais e para As partículas colidem?   "
+},
+{
+  "id": "proposition-1",
+  "level": "2",
+  "url": "section-2.html#proposition-1",
+  "type": "Proposição",
+  "number": "1.2.1",
+  "title": "",
+  "body": " Sejam funções vetoriais tais que  e uma função real, então    está bem definida e  está bem definida e  está bem definida e   está bem definida e    "
+},
+{
+  "id": "remark-3",
+  "level": "2",
+  "url": "section-2.html#remark-3",
+  "type": "Nota",
+  "number": "1.2.2",
+  "title": "",
+  "body": " Pelos itens e , se é uma constante real, temos: e Pelo item , temos ainda   "
+},
+{
+  "id": "example-7",
+  "level": "2",
+  "url": "section-2.html#example-7",
+  "type": "Exemplo",
+  "number": "1.2.3",
+  "title": "",
+  "body": " Considere dadas por e e dada por Calcule:         "
+},
+{
+  "id": "technology-4",
+  "level": "2",
+  "url": "section-2.html#technology-4",
+  "type": "Tecnologia",
+  "number": "1.2.4",
+  "title": "",
+  "body": " Usando o SageMath para calcular o item iv do exemplo anterior   "
+},
+{
+  "id": "exercise-4",
+  "level": "2",
+  "url": "section-2.html#exercise-4",
+  "type": "Exercício",
+  "number": "1.2.1",
+  "title": "",
+  "body": "Esboce a trajetória de uma partícula , sabendo que seu movimento é descrito por:       "
+},
+{
+  "id": "exercise-5",
+  "level": "2",
+  "url": "section-2.html#exercise-5",
+  "type": "Exercício",
+  "number": "1.2.2",
+  "title": "",
+  "body": "Determine o domínio das seguintes funções vetoriais:          "
+},
+{
+  "id": "exercise-6",
+  "level": "2",
+  "url": "section-2.html#exercise-6",
+  "type": "Exercício",
+  "number": "1.2.3",
+  "title": "",
+  "body": "Considere duas funções vetoriais definidas em e uma função real dada por . Determine:         "
+},
+{
+  "id": "exercise-7",
+  "level": "2",
+  "url": "section-2.html#exercise-7",
+  "type": "Exercício",
+  "number": "1.2.4",
+  "title": "",
+  "body": "Considere duas funções vetoriais definidas em Determine  "
+},
+{
+  "id": "exercise-8",
+  "level": "2",
+  "url": "section-2.html#exercise-8",
+  "type": "Exercício",
+  "number": "1.2.5",
+  "title": "",
+  "body": "Sejam Calcule  "
+},
+{
+  "id": "exercise-9",
+  "level": "2",
+  "url": "section-2.html#exercise-9",
+  "type": "Exercício",
+  "number": "1.2.6",
+  "title": "",
+  "body": "Duas partículas se movem ao longo de duas curvas espaciais e para As partículas colidem? "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
