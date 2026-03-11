a5e62f1b708b2436c64c7f0a6ffe4c3f54572d79
Ricardo <ricardo@localhost.localdomain>
2026-03-10 22:03:33 -0300

    add notas calculo nii

---
+++ notas-calculo-nii/lunr-pretext-search-index.js
@@ -0,0 +1,643 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "prof-ricardo-machado",
+  "level": "1",
+  "url": "prof-ricardo-machado.html",
+  "type": "Biografia do autor",
+  "number": "",
+  "title": "Sobre Ricardo Machado",
+  "body": " Sobre Ricardo Machado  Professor Adjunto da Universidade Federal Rural de Pernambuco, DM.  Doutor em Matemática pela Universidade Federal de Pernambuco (2009-2013).  Mestre em Matemática pela Universidade Federal de Pernambuco (2007-2009).  Bacharel em Matemática pela Universidade Federal de Pernambuco (2003-2006).  "
+},
+{
+  "id": "preface",
+  "level": "1",
+  "url": "preface.html",
+  "type": "Prefácio",
+  "number": "",
+  "title": "Prefácio",
+  "body": "    Clessius Silva  Ricardo Machado  Recife, 2022   "
+},
+{
+  "id": "section-1",
+  "level": "1",
+  "url": "section-1.html",
+  "type": "Seção",
+  "number": "1.1",
+  "title": "Integral Indefinida",
+  "body": " Integral Indefinida   Exercícios   Use o Sage para calcular as integrais propostas nos exercícios e confira com as suas respostas.    Nos itens a seguir, calcular a integral e, em seguida, derivar as respostas para conferir os resultados                 Nos itens a seguir, calcular as integrais indefinidas.                   , onde é constante.               , onde .     Encontrar uma primitiva , da função , que satisfaça .    "
+},
+{
+  "id": "technology-1",
+  "level": "2",
+  "url": "section-1.html#technology-1",
+  "type": "Tecnologia",
+  "number": "1.1.1",
+  "title": "",
+  "body": " Use o Sage para calcular as integrais propostas nos exercícios e confira com as suas respostas.   "
+},
+{
+  "id": "ex-1",
+  "level": "2",
+  "url": "section-1.html#ex-1",
+  "type": "Exercício",
+  "number": "1.1.1",
+  "title": "",
+  "body": "Nos itens a seguir, calcular a integral e, em seguida, derivar as respostas para conferir os resultados              "
+},
+{
+  "id": "ex-11",
+  "level": "2",
+  "url": "section-1.html#ex-11",
+  "type": "Exercício",
+  "number": "1.1.2",
+  "title": "",
+  "body": "Nos itens a seguir, calcular as integrais indefinidas.                   , onde é constante.               , onde .   "
+},
+{
+  "id": "exercise-3",
+  "level": "2",
+  "url": "section-1.html#exercise-3",
+  "type": "Exercício",
+  "number": "1.1.3",
+  "title": "",
+  "body": " Encontrar uma primitiva , da função , que satisfaça .  "
+},
+{
+  "id": "sec-metodo-substituicao",
+  "level": "1",
+  "url": "sec-metodo-substituicao.html",
+  "type": "Seção",
+  "number": "1.2",
+  "title": "Mudança de Variável para Integração",
+  "body": " Mudança de Variável para Integração     Seja uma primitiva de , ou seja, . Suponhamos que seja uma função derivável tal que a imagem de esteja contida no domínio de . Então, a integral da função composta multiplicada pela derivada da função interna é dada por: .    Consideremos a função composta . Pela regra da cadeia para derivação, temos que a derivada dessa composição é: .  Como, por hipótese, , podemos substituir por na expressão anterior, obtendo: .  Por definição de primitiva, isso significa que é uma primitiva de . Portanto: .  Para simplificar o cálculo na prática, fazemos a mudança de variável , o que implica no diferencial . Substituindo esses termos em , a integral assume a forma básica: .    Na prática, o objetivo é definir uma função conveniente, de tal forma que a integral obtida seja mais simples que a original.   Exemplos  Calcule as integrais:    .   Fazemos . Então, . Temos: .         Se fizermos , então . Assim: .      .   Fazendo , temos . Então, .         Podemos escrever . Fazendo , temos e então . Portanto, .      .   Fazendo , temos ou . Portanto, .      .   Podemos escrever: . Para resolver , fazemos , . Obtemos: .          Completando o quadrado: . Fazendo , obtemos .      Exercícios   Use o Sage para calcular as integrais propostas nos exercícios e confira com as suas respostas.     Calcular as integrais seguintes usando o método da substituição.                                                                                                     "
+},
+{
+  "id": "thm-mudanca-variavel",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#thm-mudanca-variavel",
+  "type": "Teorema",
+  "number": "1.2.1",
+  "title": "",
+  "body": "   Seja uma primitiva de , ou seja, . Suponhamos que seja uma função derivável tal que a imagem de esteja contida no domínio de . Então, a integral da função composta multiplicada pela derivada da função interna é dada por: .    Consideremos a função composta . Pela regra da cadeia para derivação, temos que a derivada dessa composição é: .  Como, por hipótese, , podemos substituir por na expressão anterior, obtendo: .  Por definição de primitiva, isso significa que é uma primitiva de . Portanto: .  Para simplificar o cálculo na prática, fazemos a mudança de variável , o que implica no diferencial . Substituindo esses termos em , a integral assume a forma básica: .   "
+},
+{
+  "id": "example-1",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#example-1",
+  "type": "Exemplo",
+  "number": "1.2.2",
+  "title": "",
+  "body": "  .   Fazemos . Então, . Temos: .   "
+},
+{
+  "id": "example-2",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#example-2",
+  "type": "Exemplo",
+  "number": "1.2.3",
+  "title": "",
+  "body": "     Se fizermos , então . Assim: .   "
+},
+{
+  "id": "example-3",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#example-3",
+  "type": "Exemplo",
+  "number": "1.2.4",
+  "title": "",
+  "body": "  .   Fazendo , temos . Então, .   "
+},
+{
+  "id": "example-4",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#example-4",
+  "type": "Exemplo",
+  "number": "1.2.5",
+  "title": "",
+  "body": "     Podemos escrever . Fazendo , temos e então . Portanto, .   "
+},
+{
+  "id": "example-5",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#example-5",
+  "type": "Exemplo",
+  "number": "1.2.6",
+  "title": "",
+  "body": "  .   Fazendo , temos ou . Portanto, .   "
+},
+{
+  "id": "example-6",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#example-6",
+  "type": "Exemplo",
+  "number": "1.2.7",
+  "title": "",
+  "body": "  .   Podemos escrever: . Para resolver , fazemos , . Obtemos: .   "
+},
+{
+  "id": "example-7",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#example-7",
+  "type": "Exemplo",
+  "number": "1.2.8",
+  "title": "",
+  "body": "     Completando o quadrado: . Fazendo , obtemos .   "
+},
+{
+  "id": "technology-2",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#technology-2",
+  "type": "Tecnologia",
+  "number": "1.2.9",
+  "title": "",
+  "body": " Use o Sage para calcular as integrais propostas nos exercícios e confira com as suas respostas.   "
+},
+{
+  "id": "exercise-4",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-4",
+  "type": "Exercício",
+  "number": "1.2.2.1",
+  "title": "",
+  "body": ""
+},
+{
+  "id": "exercise-5",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-5",
+  "type": "Exercício",
+  "number": "1.2.2.2",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-6",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-6",
+  "type": "Exercício",
+  "number": "1.2.2.3",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-7",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-7",
+  "type": "Exercício",
+  "number": "1.2.2.4",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-8",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-8",
+  "type": "Exercício",
+  "number": "1.2.2.5",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-9",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-9",
+  "type": "Exercício",
+  "number": "1.2.2.6",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-10",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-10",
+  "type": "Exercício",
+  "number": "1.2.2.7",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-11",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-11",
+  "type": "Exercício",
+  "number": "1.2.2.8",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-12",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-12",
+  "type": "Exercício",
+  "number": "1.2.2.9",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-13",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-13",
+  "type": "Exercício",
+  "number": "1.2.2.10",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-14",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-14",
+  "type": "Exercício",
+  "number": "1.2.2.11",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-15",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-15",
+  "type": "Exercício",
+  "number": "1.2.2.12",
+  "title": "",
+  "body": ""
+},
+{
+  "id": "exercise-16",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-16",
+  "type": "Exercício",
+  "number": "1.2.2.13",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-17",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-17",
+  "type": "Exercício",
+  "number": "1.2.2.14",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-18",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-18",
+  "type": "Exercício",
+  "number": "1.2.2.15",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-19",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-19",
+  "type": "Exercício",
+  "number": "1.2.2.16",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-20",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-20",
+  "type": "Exercício",
+  "number": "1.2.2.17",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-21",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-21",
+  "type": "Exercício",
+  "number": "1.2.2.18",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-22",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-22",
+  "type": "Exercício",
+  "number": "1.2.2.19",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-23",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-23",
+  "type": "Exercício",
+  "number": "1.2.2.20",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-24",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-24",
+  "type": "Exercício",
+  "number": "1.2.2.21",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-25",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-25",
+  "type": "Exercício",
+  "number": "1.2.2.22",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-26",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-26",
+  "type": "Exercício",
+  "number": "1.2.2.23",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-27",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-27",
+  "type": "Exercício",
+  "number": "1.2.2.24",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-28",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-28",
+  "type": "Exercício",
+  "number": "1.2.2.25",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-29",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-29",
+  "type": "Exercício",
+  "number": "1.2.2.26",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-30",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-30",
+  "type": "Exercício",
+  "number": "1.2.2.27",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-31",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-31",
+  "type": "Exercício",
+  "number": "1.2.2.28",
+  "title": "",
+  "body": ""
+},
+{
+  "id": "exercise-32",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-32",
+  "type": "Exercício",
+  "number": "1.2.2.29",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-33",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-33",
+  "type": "Exercício",
+  "number": "1.2.2.30",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-34",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-34",
+  "type": "Exercício",
+  "number": "1.2.2.31",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-35",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-35",
+  "type": "Exercício",
+  "number": "1.2.2.32",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-36",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-36",
+  "type": "Exercício",
+  "number": "1.2.2.33",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-37",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-37",
+  "type": "Exercício",
+  "number": "1.2.2.34",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-38",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-38",
+  "type": "Exercício",
+  "number": "1.2.2.35",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-39",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-39",
+  "type": "Exercício",
+  "number": "1.2.2.36",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-40",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-40",
+  "type": "Exercício",
+  "number": "1.2.2.37",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-41",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-41",
+  "type": "Exercício",
+  "number": "1.2.2.38",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-42",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-42",
+  "type": "Exercício",
+  "number": "1.2.2.39",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-43",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-43",
+  "type": "Exercício",
+  "number": "1.2.2.40",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-44",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-44",
+  "type": "Exercício",
+  "number": "1.2.2.41",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-45",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-45",
+  "type": "Exercício",
+  "number": "1.2.2.42",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-46",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-46",
+  "type": "Exercício",
+  "number": "1.2.2.43",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-47",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-47",
+  "type": "Exercício",
+  "number": "1.2.2.44",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-48",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-48",
+  "type": "Exercício",
+  "number": "1.2.2.45",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-49",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-49",
+  "type": "Exercício",
+  "number": "1.2.2.46",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-50",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-50",
+  "type": "Exercício",
+  "number": "1.2.2.47",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-51",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-51",
+  "type": "Exercício",
+  "number": "1.2.2.48",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-52",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-52",
+  "type": "Exercício",
+  "number": "1.2.2.49",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "exercise-53",
+  "level": "2",
+  "url": "sec-metodo-substituicao.html#exercise-53",
+  "type": "Exercício",
+  "number": "1.2.2.50",
+  "title": "",
+  "body": " "
+},
+{
+  "id": "referencias",
+  "level": "1",
+  "url": "referencias.html",
+  "type": "Referêcias",
+  "number": "2",
+  "title": "Referências Bibliográficas",
+  "body": "    Referências Bibliográficas   Livros    Guidorizzi, Hamilton Luiz, Cálculo , volume 1, LTC, 5ª edição, 2001.    Lima, Elon Lages, Análise Real , volume 1, Projeto Euclides, 2008.    LIMA, Elon Lages et al., A matemática do ensino médio , volume 3, Coleção do Professor de Matemática, SBM, 7ª ed, 2016.    WAGNER, Eduardo; MORGADO, Augusto Cesar de O.; CARMO, M., Trigonometria e números complexos , Coleção do Professor de Matemática, SBM, 1992.  Artigos  "
+},
+{
+  "id": "p-106",
+  "level": "2",
+  "url": "referencias.html#p-106",
+  "type": "Parágrafo (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Livros "
+},
+{
+  "id": "p-107",
+  "level": "2",
+  "url": "referencias.html#p-107",
+  "type": "Parágrafo (with a defined term)",
+  "number": "",
+  "title": "",
+  "body": "Artigos "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
